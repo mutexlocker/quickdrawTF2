acb9dc2c13218e97d7c49e989ba280d8e94f93bb
Nima Aghli <naghli2014@my.fit.edu>
2019-04-15 21:53:55 -0400

    fix data

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
 import numpy as np
 import os
+import sys
+
 from sklearn.model_selection import train_test_split, GridSearchCV
+from matplotlib import pyplot as plt
+np.set_printoptions(threshold=2000)
+
+
 dir = "data/"
 class_count = len(os.listdir(dir))
 data = []
 classcounter = 0
 totalclasses = 50
-samples = 5000
+samples = 10000
 label_dict = {}
 for filename in os.listdir(dir)[:totalclasses]:
     tmp = np.load(dir + filename)
+    #print(tmp[1,:])
     tmp = np.c_[tmp, classcounter * np.ones(len(tmp))]
     label_dict.update({filename.replace(".npy","") : classcounter})
     print("add to dict" , filename.replace(".npy",""), classcounter)
@@ -78,22 +85,28 @@ num_classes = y_test_cnn.shape[1]
 
 # # reshape to be [samples][pixels][width][height]
 X_train_cnn = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
+# print(X_train_cnn[9500].shape)
+# print(y_train_cnn[9500])
+# plt.imshow(X_train_cnn[9500].reshape(28,28), cmap='gray')
+# plt.savefig("test.png")
 X_test_cnn = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
 # define the CNN model
 def cnn_model():
     # create model
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(80, (8, 8), input_shape=(28, 28, 1),activation = 'relu',kernel_initializer='random_uniform'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.Conv2D(15, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(40, (4, 4), activation = 'relu',kernel_initializer='random_uniform'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Conv2D(20, (2, 2), activation = 'relu',kernel_initializer='random_uniform'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(50, activation='relu'))
-    model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+    model.add(tf.keras.layers.Dense(512,activation = 'relu',kernel_initializer='random_uniform'))
+    model.add(tf.keras.layers.Dense(256, activation = 'relu',kernel_initializer='random_uniform'))
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax',kernel_initializer='random_uniform'))
     # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'top_k_categorical_accuracy'])
     return model
 
 
@@ -101,7 +114,17 @@ np.random.seed(0)
 # build the model
 model_cnn = cnn_model()
 # Fit the model
-model_cnn.fit(X_train_cnn, y_train_cnn, validation_data=(X_test_cnn, y_test_cnn), epochs=10, batch_size=200)
+model_cnn.fit(X_train_cnn, y_train_cnn, validation_data=(X_test_cnn, y_test_cnn), epochs=20, batch_size=200)
 # Final evaluation of the model
 scores = model_cnn.evaluate(X_test_cnn, y_test_cnn, verbose=0)
+
 print('Final CNN accuracy: ', scores[1])
+print(scores[2])
+print(scores[0])
+model_json = model_cnn.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model_cnn.save_weights("model.h5")
+print("Saved model to disk")
+
