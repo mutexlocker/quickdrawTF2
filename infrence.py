de3643d8be29175a1c827c78b98d8cf5b0f2ac5b
Nima Aghli <naghli2014@my.fit.edu>
2019-04-23 16:06:32 -0400

    100 class

--- infrence.py
+++ infrence.py
@@ -6,27 +6,27 @@ import matplotlib.pyplot as plt
 
 
 
-# tmp = Image.open('hat.png').convert('L')
-# tmp = PIL.ImageOps.invert(tmp)
-# tmp = np.asarray(tmp)
-# print(tmp)
+tmp = Image.open('hat.png').convert('L')
+tmp = PIL.ImageOps.invert(tmp)
+tmp = np.asarray(tmp)/255.
+print(tmp)
 
-tmp = np.load("data/star.npy")
-tmp = tmp[1]
+# tmp = np.load("data/star.npy")
+# tmp = tmp[9]/255.
 
 print(tmp)
 print(tmp.shape)
 plt.imshow(tmp.reshape(28,28),cmap='gray')
 plt.savefig("test.png")
-tmp = tmp.reshape(1,28, 28,1).astype('float32')
+tmp = tmp.reshape(1,28, 28).astype('float32')
 
 
 # Model reconstruction from JSON file
-with open('model_cnn.json', 'r') as f:
+with open('model_cnn_lstm.json', 'r') as f:
     model = tf.keras.models.model_from_json(f.read())
 
 # Load weights into the new model
-model.load_weights('model_cnn.h5')
+model.load_weights('model_cnn_lstm.h5')
 print(model.summary())
 np.set_printoptions(suppress=True)
 tem_res = model.predict(tmp)
--- main.py
+++ main.py
@@ -18,8 +18,12 @@ dir = "data/"
 class_count = len(os.listdir(dir))
 data = []
 classcounter = 0
-totalclasses = 50
-samples = 10000
+totalclasses = 100
+for filename in os.listdir(dir)[:totalclasses]:
+    print("add to dict" , filename.replace(".npy",""), classcounter)
+    classcounter += 1
+classcounter = 0
+samples = 15000
 label_dict = {}
 for filename in os.listdir(dir)[:totalclasses]:
     tmp = np.load(dir + filename)
@@ -80,48 +84,58 @@ print(y.shape)
 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X/255.,y,test_size=0.3,random_state=100)
+X_train, X_test, y_train, y_test = train_test_split(X/255.,y,test_size=0.4,random_state=100)
+X_test, X_realtest, y_test, y_realtest = train_test_split(X_test,y_test,test_size=0.3,random_state=100)
+
 
 y_train_cnn = tf.keras.utils.to_categorical(y_train)
 y_test_cnn =  tf.keras.utils.to_categorical(y_test)
+y_realtest_cnn = tf.keras.utils.to_categorical(y_realtest)
 num_classes = y_test_cnn.shape[1]
+print("X_train shape is :", X_train.shape)
+print("X_test shape is :", X_test.shape)
+print("X_realtest shape is :", X_realtest.shape)
+
 print("y_train shape is :", y_train_cnn.shape)
 print("y_test shape is :", y_test_cnn.shape)
-
+print("y_realtest shape is :", y_realtest_cnn.shape)
 
 
 # # reshape to be [samples][pixels][width][height]
-X_train_cnn = X_train.reshape(X_train.shape[0], 28, 28,1).astype('float32')
+
 # print(X_train_cnn.shape)
 # print(y_train_cnn[9500])
 # plt.imshow(X_train_cnn[9500].reshape(28,28), cmap='gray')
 # plt.savefig("test.png")
+X_train_cnn = X_train.reshape(X_train.shape[0], 28, 28,1).astype('float32')
 X_test_cnn = X_test.reshape(X_test.shape[0], 28, 28,1).astype('float32')
-print("xtrain shape is :", X_train_cnn.shape)
-print("xtest shape is :", X_test_cnn.shape)
+X_realtest_cnn = X_realtest.reshape(X_realtest.shape[0], 28, 28,1).astype('float32')
+print("x_train_cnn shape is :", X_train_cnn.shape)
+print("x_test_cnn shape is :", X_test_cnn.shape)
+print("x_realtest_cnn shape is :", X_realtest_cnn.shape)
+print(X_realtest[0])
 # define the CNN model
 def cnn_model3():
     # create model
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), input_shape=(28, 28, 1),activation = 'relu',kernel_initializer='TruncatedNormal'))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), input_shape=(28, 28, 1),activation = 'relu',kernel_initializer='RandomUniform'))
     BatchNormalization(axis=-1)
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu',kernel_initializer='TruncatedNormal'))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu',kernel_initializer='RandomUniform'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
     BatchNormalization(axis=-1)
-    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu',kernel_initializer='TruncatedNormal'))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu',kernel_initializer='RandomUniform'))
     BatchNormalization(axis=-1)
-    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='TruncatedNormal'))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='RandomUniform'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
     model.add(tf.keras.layers.Flatten())
     BatchNormalization()
-    model.add(tf.keras.layers.Dense(512,activation = 'relu',kernel_initializer='TruncatedNormal'))
-    model.add(tf.keras.layers.Dropout(0.3))
-    model.add(tf.keras.layers.Dense(128, activation = 'tanh',kernel_initializer='TruncatedNormal'))
-    model.add(tf.keras.layers.Dropout(0.3))
-    model.add(tf.keras.layers.Dense(num_classes, activation='softmax',kernel_initializer='TruncatedNormal'))
+    model.add(tf.keras.layers.Dense(512,activation = 'relu',kernel_initializer='RandomUniform'))
+    BatchNormalization()
+    model.add(tf.keras.layers.Dense(128, activation = 'relu',kernel_initializer='RandomUniform'))
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax',kernel_initializer='RandomUniform'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'top_k_categorical_accuracy'])
-    tensorboard = TensorBoard(log_dir="logs/{}".format(time()),histogram_freq=1,
+    tensorboard = TensorBoard(log_dir="logs/cnn_model3_deep_{}".format(time()),histogram_freq=1,
           write_graph = True, write_images = True,write_grads = True, batch_size = 100)
     return model,tensorboard
 
@@ -163,32 +177,33 @@ def cnn_model1():
     return model
 def cnn_model0():
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu',kernel_initializer='RandomUniform'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.Conv2D(15, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(15, (3, 3), activation='relu',kernel_initializer='RandomUniform'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dropout(0.3))
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(512, activation='relu'))
-    model.add(tf.keras.layers.Dense(256, activation='relu'))
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+    model.add(tf.keras.layers.Dense(256, activation='relu',kernel_initializer='RandomUniform'))
+    model.add(tf.keras.layers.Dense(128, activation='relu',kernel_initializer='RandomUniform'))
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax',kernel_initializer='RandomUniform'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'top_k_categorical_accuracy'])
-    return model
+    tensorboard = TensorBoard(log_dir="logs/cnn_model0{}".format(time()),histogram_freq=1,
+          write_graph = True, write_images = True,write_grads = True)
+    return model,tensorboard
 
 
 def cnn_model_lstm():
     model = tf.keras.Sequential()
-    model.add(LSTM(128, activation='tanh',input_shape=(28, 28), return_sequences=True))
-    model.add(LSTM(256, activation='tanh'))
+    model.add(LSTM(128, activation='relu',input_shape=(28, 28), return_sequences=True))
+    model.add(LSTM(256, activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Dense(256, activation='tanh'))
+    model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.2))
-    model.add(Dense(64, activation='tanh'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'top_k_categorical_accuracy'])
-    tensorboard = TensorBoard(log_dir="logs/{}".format(time()),histogram_freq=1,
+    tensorboard = TensorBoard(log_dir="logs/LSTM_{}".format(time()),histogram_freq=1,
           write_graph = True, write_images = True,write_grads = True, batch_size = 100)
     model.summary()
     return model,tensorboard
@@ -218,17 +233,18 @@ np.random.seed(0)
 model_cnn,tensorboard = cnn_model3()
 model_cnn.summary()
 # Fit the model
-model_cnn.fit(X_train_cnn, y_train_cnn, validation_data=(X_test_cnn, y_test_cnn), epochs=10, batch_size=100, callbacks=[tensorboard])
+model_cnn.fit(X_train_cnn, y_train_cnn, validation_data=(X_test_cnn, y_test_cnn), epochs=14, batch_size=200, callbacks=[tensorboard])
 # Final evaluation of the model
 scores = model_cnn.evaluate(X_test_cnn, y_test_cnn, verbose=0)
+print('Final Validation accuracy: ', scores[1])
+scores = model_cnn.evaluate(X_realtest_cnn, y_realtest_cnn, verbose=0)
+print('Final test accuracy: ', scores[1])
 
-print('Final CNN accuracy: ', scores[1])
-print(scores[2])
-print(scores[0])
+
 model_json = model_cnn.to_json()
-with open("model_cnn.json", "w") as json_file:
+with open("model_cnn_deep.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model_cnn.save_weights("model_cnn.h5")
+model_cnn.save_weights("model_cnn_deep.h5")
 print("Saved model to disk")
 
